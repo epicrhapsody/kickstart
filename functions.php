e310d70814c6c0c0a836a41f2ef11d9a3baf93b0
Karsten vom Wege <mail@vomwege.de>
2016-01-26 15:42:20 +0100

    v1.5

---
+++ functions.php
@@ -0,0 +1,290 @@
+<?php 
+
+if (!is_admin())
+    add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+
+function my_jquery_enqueue() {
+    wp_deregister_script('jquery');
+    $googleScript = 'http'. ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . '://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
+    wp_register_script('jquery', $googleScript, false, '1.11.1', true);
+    wp_enqueue_script('jquery');
+}
+
+add_theme_support( 'html5', array( 'search-form' ) );
+
+function custom_excerpt_length( $length ) {
+	return 16;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 20 );
+
+function search_excerpt_highlight() {
+    $excerpt = get_the_excerpt();
+    $keys = implode('|', explode(' ', get_search_query()));
+    $excerpt = preg_replace('/(' . $keys .')/iu', '<strong class="search-highlight">\0</strong>', $excerpt);
+
+    echo '<p>' . $excerpt . '</p>';
+}
+
+function search_title_highlight() {
+    $title = get_the_title();
+    $keys = implode('|', explode(' ', get_search_query()));
+    $title = preg_replace('/(' . $keys .')/iu', '<strong class="search-highlight">\0</strong>', $title);
+
+    echo $title;
+}
+
+function add_title_attachment_link($link, $id = null) {
+	$id = intval( $id );
+	$_post = get_post( $id );
+	$post_title = esc_attr( $_post->post_title );
+	return str_replace('<a href', '<a title="'. $post_title .'" href', $link);
+}
+add_filter('wp_get_attachment_link', 'add_title_attachment_link', 10, 2);
+
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/t.png);
+            background-size: 100% auto;
+            background-position: center center;
+            height: 70px;width: 250px;
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+add_theme_support( 'post-thumbnails' );
+
+add_action( 'after_setup_theme', 'baw_theme_setup' );
+function baw_theme_setup() {
+  add_image_size( 'category-thumb', 300 ); // 300 pixels wide (and unlimited height)
+}
+
+add_theme_support( 'post-formats', array(
+	'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+) );
+
+add_action('set_current_user', 'cc_hide_admin_bar');
+function cc_hide_admin_bar() {
+  if (!current_user_can('edit_posts')) {
+    show_admin_bar(false);
+  }
+}
+
+register_sidebar( array(
+	'name' => __( 'Homepage' ),
+	'id' => 'homepage',
+	'description' => __( 'Startseite'),
+	'before_widget' => '',
+	'after_widget'  => '',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+function sButton($atts, $content = null) {
+   extract(shortcode_atts(array('link' => '#'), $atts));
+   return '<a class="button" href="'.$link.'"><span>' . do_shortcode($content) . '</span></a>';
+}
+add_shortcode('button', 'sButton');
+
+add_filter('widget_text', 'do_shortcode');
+
+
+register_nav_menus( array(
+	'primary'   => __( 'Top primary menu', 'twentyfourteen' ),
+	'secondary'   => __( 'Bottom secondary menu', 'twentyfourteen' ),
+) );
+
+// Thumbnails and Image support
+
+if ( function_exists( 'add_theme_support' ) ) { 
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
+
+    // additional image sizes
+    // delete the next line if you do not need additional image sizes
+    add_image_size( '320-thumb', 320, 9999, false );
+    add_image_size( '375-thumb', 375, 9999, false );
+    add_image_size( '414-thumb', 414, 9999, false );
+    add_image_size( '768-thumb', 768, 9999, false );
+    add_image_size( '900-thumb', 900, 9999, false );
+    add_image_size( '1024-thumb', 1024, 9999, false );
+    add_image_size( '1140-thumb', 1140, 9999, false );
+}
+
+// Gallery as list
+
+// Remove built in shortcode
+remove_shortcode('gallery', 'gallery_shortcode');
+
+// Replace with custom shortcode
+function shortcode_gallery($attr) {
+  $post = get_post();
+
+  static $instance = 0;
+  $instance++;
+
+  if (!empty($attr['ids'])) {
+    if (empty($attr['orderby'])) {
+      $attr['orderby'] = 'post__in';
+    }
+    $attr['include'] = $attr['ids'];
+  }
+
+  $output = apply_filters('post_gallery', '', $attr);
+
+  if ($output != '') {
+    return $output;
+  }
+
+  if (isset($attr['orderby'])) {
+    $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+    if (!$attr['orderby']) {
+      unset($attr['orderby']);
+    }
+  }
+
+  extract(shortcode_atts(array(
+    'order'      => 'ASC',
+    'orderby'    => 'menu_order ID',
+    'id'         => $post->ID,
+    'itemtag'    => '',
+    'icontag'    => '',
+    'captiontag' => '',
+    'columns'    => 1,
+    'size'       => 'thumbnail',
+    'include'    => '',
+    'exclude'    => ''
+  ), $attr));
+
+  $id = intval($id);
+
+  if ($order === 'RAND') {
+    $orderby = 'none';
+  }
+
+  if (!empty($include)) {
+    $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+    $attachments = array();
+    foreach ($_attachments as $key => $val) {
+      $attachments[$val->ID] = $_attachments[$key];
+    }
+  } elseif (!empty($exclude)) {
+    $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+  } else {
+    $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+  }
+
+  if (empty($attachments)) {
+    return '';
+  }
+
+  if (is_feed()) {
+    $output = "\n";
+    foreach ($attachments as $att_id => $attachment) {
+      $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+    }
+    return $output;
+  }
+ 
+ $output = '<ul class="gallery clearfix">';
+  $i = 0;
+  foreach ($attachments as $id => $attachment) {
+    $link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+    $output .= '<li>' . $link;
+    if (trim($attachment->post_excerpt)) {
+      $output .= '<div class="caption">' . wptexturize($attachment->post_excerpt) . '</div>';
+    }
+    $output .= '</li>';
+  }
+
+  $output .= '</ul>';
+  return $output;
+}
+add_shortcode('gallery', 'shortcode_gallery');
+
+function pagination($pages = '', $range = 4)
+{  
+     $showitems = ($range * 2)+1;  
+ 
+     global $paged;
+     if(empty($paged)) $paged = 1;
+ 
+     if($pages == '')
+     {
+         global $wp_query;
+         $pages = $wp_query->max_num_pages;
+         if(!$pages)
+         {
+             $pages = 1;
+         }
+     }   
+ 
+     if(1 != $pages)
+     {
+         echo "<div class=\"pagination\">";
+         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo; First</a>";
+         if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo; Previous</a>";
+ 
+         for ($i=1; $i <= $pages; $i++)
+         {
+             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+             {
+                 echo ($paged == $i)? "<span class=\"current\">".$i."</span>":"<a href='".get_pagenum_link($i)."' class=\"inactive\">".$i."</a>";
+             }
+         }
+ 
+         if ($paged < $pages && $showitems < $pages) echo "<a href=\"".get_pagenum_link($paged + 1)."\">Next &rsaquo;</a>";  
+         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>Last &raquo;</a>";
+         echo "</div>\n";
+     }
+}
+
+//Clean up Wordpress
+	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+	remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+add_filter('show_admin_bar', '__return_false');
+add_filter( 'use_default_gallery_style', '__return_false' );
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+@ini_set( 'upload_max_size' , '128M' );
+@ini_set( 'post_max_size', '128M');
+@ini_set( 'max_execution_time', '300' );
+ ?>
